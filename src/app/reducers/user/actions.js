8587f2d2938e3ef20d31ce7b411c0f0fee5c5b2f
evuz <jesus_jgb_45@hotmail.com>
2017-07-19 22:31:04 +0200

    Go /user when do login

--- src/app/reducers/user/actions.js
+++ src/app/reducers/user/actions.js
@@ -1,5 +1,7 @@
-import { SET_USER } from './actionTypes';
 import firebase from 'firebase';
+import { push } from 'react-router-redux';
+
+import { SET_USER } from './actionTypes';
 
 export function setUser(user) {
   return {
@@ -21,6 +23,7 @@ export function loginGoogle() {
           email: res.user.email,
           photoURL: res.user.photoURL
         }))
+        dispatch(push('/user'));
       }).catch(function (error) {
         throw new Error(error)
       });
