28db9b5faa1008f4ddc7e54014f77ae70f20c753
evuz <jesus_jgb_45@hotmail.com>
2017-07-19 20:33:27 +0200

    Route: create route /user

--- src/app/containers/Route.js
+++ src/app/containers/Route.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import IntroComponent from '../components/Intro';
+import UserDataComponent from './UserData';
 
 const RouteContainer = () => (
   <Switch>
     <Route path="/" exact component={IntroComponent} />
+    <Route path="/user" exact component={UserDataComponent} />
   </Switch>
 );
 
