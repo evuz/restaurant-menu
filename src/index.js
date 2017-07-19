6e674b0784e81b1f3acc167a3ed0da6b06849ba2
evuz <jesus_jgb_45@hotmail.com>
2017-07-19 20:12:56 +0200

    Configure firebase

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
+import firebase from 'firebase';
 
 import configureStore from './configureStore';
+import configFirebase from './configFirebase';
 import registerServiceWorker from './registerServiceWorker';
 
 import App from './app/App';
@@ -11,6 +13,7 @@ import App from './app/App';
 import './index.css';
 
 const { store, history } = configureStore();
+firebase.initializeApp(configFirebase);
 
 ReactDOM.render(
   <Provider store={store}>
