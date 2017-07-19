2f4085d9ee6e676e8dcf42975a03a8631bd73fc9
evuz <jesus_jgb_45@hotmail.com>
2017-07-19 23:59:54 +0200

    Header component: style

--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -5,9 +5,9 @@ import logo from '../../assets/logo.svg';
 import './index.css';
 
 const HeaderComponent = ({ onClickImg, img }) => (
-  <div className="App-header">
+  <div className="header_component">
     <img src={img || logo}
-      className={img ? 'App-img' : 'App-logo'}
+      className={img ? 'header_component_img' : 'header_component_logo'}
       alt="logo"
       onClick={onClickImg}
     />
