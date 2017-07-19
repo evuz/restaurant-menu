37dfe2aa4037be5c08fd42eaef713861f307bbff
evuz <jesus_jgb_45@hotmail.com>
2017-07-19 23:08:40 +0200

    Route: create new route to see example form

--- src/app/containers/Route.js
+++ src/app/containers/Route.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import IntroComponent from '../components/Intro';
 import UserDataComponent from './UserData';
+import ExampleFormContainer from './ExampleFormContainer';
 
 const RouteContainer = () => (
   <Switch>
     <Route path="/" exact component={IntroComponent} />
     <Route path="/user" exact component={UserDataComponent} />
+    <Route path="/form" exact component={ExampleFormContainer} />
   </Switch>
 );
 
