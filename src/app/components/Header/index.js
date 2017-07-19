bbca1123bc220bf361d4beb7c7d722b7ad074e7f
evuz <jesus_jgb_45@hotmail.com>
2017-07-19 20:13:28 +0200

    Header component: event click on logo

--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -4,9 +4,13 @@ import logo from '../../assets/logo.svg';
 
 import './index.css';
 
-const HeaderComponent = () => (
+const HeaderComponent = ({ onClickImg }) => (
   <div className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
+    <img src={logo}
+      className="App-logo"
+      alt="logo"
+      onClick={onClickImg}
+    />
     <h2>Welcome to React</h2>
   </div>
 );
