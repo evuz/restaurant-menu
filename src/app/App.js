f231852b389566937baf6a5fd57d84c6733e86db
evuz <jesus_jgb_45@hotmail.com>
2017-07-19 20:31:27 +0200

    App: user Header container

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import HeaderComponent from './components/Header';
+import HeaderContainer from './containers/Header';
 import Route from './containers/Route';
 
 import './App.css';
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <HeaderComponent />
+        <HeaderContainer />
         <Route />
       </div>
     );
