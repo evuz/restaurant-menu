a0d242a7829dfe887bc2e95d377f9bfab9c0303b
evuz <jesus_jgb_45@hotmail.com>
2017-07-20 20:51:07 +0200

    Route: create route /restaurants

--- src/app/containers/Route.js
+++ src/app/containers/Route.js
@@ -4,12 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import IntroComponent from '../components/Intro';
 import UserDataComponent from './UserData';
 import CreateRestaurantFormContainer from './CreateRestaurantFormContainer';
+import RestaurantListContainer from './RestaurantList';
 
 const RouteContainer = () => (
   <Switch>
     <Route path="/" exact component={IntroComponent} />
     <Route path="/user" exact component={UserDataComponent} />
     <Route path="/createRestaurant" exact component={CreateRestaurantFormContainer} />
+    <Route path="/restaurants" exact component={RestaurantListContainer} />
   </Switch>
 );
 
