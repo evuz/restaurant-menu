486cad25058a7362196c61d07dc90075694038b3
evuz <jesus_jgb_45@hotmail.com>
2017-07-21 13:24:43 +0200

    CleanString filter

---
+++ src/app/utils/filters/cleanString.js
@@ -0,0 +1,21 @@
+export function getCleanedString(cadena) {
+  // Definimos los caracteres que queremos eliminar
+  var specialChars = '! @#$^&%*()+=-[]\\/{}|:<>¿?,.';
+
+  // Los eliminamos todos
+  for (var i = 0; i < specialChars.length; i++) {
+    cadena = cadena.replace(new RegExp('\\' + specialChars[i], 'gi'), '');
+  }
+
+  // Lo queremos devolver limpio en minusculas
+  cadena = cadena.toLowerCase();
+
+  // Quitamos acentos y 'ñ'. Fijate en que va sin comillas el primer parametro
+  cadena = cadena.replace(/á/gi, 'a');
+  cadena = cadena.replace(/é/gi, 'e');
+  cadena = cadena.replace(/í/gi, 'i');
+  cadena = cadena.replace(/ó/gi, 'o');
+  cadena = cadena.replace(/ú/gi, 'u');
+  cadena = cadena.replace(/ñ/gi, 'n');
+  return cadena;
+}
